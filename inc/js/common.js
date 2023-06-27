2a353b5daae2991256cb55c33fa04ab3c48eee90
hr-park <hrpark@neungyule.com>
2023-06-27 17:40:19 +0900

    test

--- inc/js/common.js
+++ inc/js/common.js
@@ -1,25 +1,55 @@
-function gnbOpen(){
-	$('.gnb').addClass('active');
-	$('.gnb .dep2_list').show();
-	$('.btn_gnb_open').hide();
-	$('.btn_gnb_close').show();
+function gnbTogglePc(){
+	$('.gnb').toggleClass('active');
+	$('.gnb .dep2_list').toggle();
+	$('.gnb .btn_gnb').toggleClass('active');
 }
-function gnbClose(){
-	$('.gnb').removeClass('active');
-	$('.gnb .dep2_list').hide();
-	$('.btn_gnb_open').show();
-	$('.btn_gnb_close').hide();
+
+
+function gnbOpenMo(){
+	$('.nav_mo').addClass('active');
 }
-$(function(){
-	//gnb오픈아닐때..등등
-	$('.gnb .dep1_list li a').mouseover(function(){
-		//$('.gnb').addClass('active');
+
+function gnbCloseMo(){
+	$('.nav_mo').removeClass('active');
+}
+
+function gnbOverPc(){
+	if(!$('.gnb.active').length==1){
+		$('.gnb .dep1_list > li').mouseover(function(){
+			$(this).find('.dep2_list').addClass('active');
+		});
+		$('.gnb .dep1_list > li').mouseleave(function(){
+			$('.gnb .dep2_list').removeClass('active');
+		});
+	}
+}
+
+function gnbMo(){
+	$('.gnb .dep1_list li a').on('click', function(){
+		$('.gnb .dep2_list').removeClass('active');
 		$(this).next('.dep2_list').addClass('active');
 	});
-	//$('.gnb .dep1_list li a').mouseleave(function(){
-	$('.gnb').mouseleave(function(){
-		$('.gnb').removeClass('active');
-		//$('.gnb .dep1_list li').children('a').removeClass('on');
-		$('.gnb .dep2_list').removeClass('active');
+}
+
+$(function(){
+	let vh = window.innerHeight * 0.01
+	document.documentElement.style.setProperty('--vh', vh + 'px')
+	$(window).resize(function(){
+		let vh = window.innerHeight * 0.01
+		document.documentElement.style.setProperty('--vh', vh + 'px')
 	});
+
+	if(matchMedia("screen and (min-width:768px)").matches) {
+		gnbOverPc();
+	}
+
+	$(window).resize(function() {
+		if(matchMedia("screen and (min-width:768px)").matches){
+			gnbOverPc();
+		}else{
+			//gnbOverPc();
+			$('.gnb, .gnb .dep1_list li a').off('mouseover mouseleave');
+			gnbMo();
+		}
+	}).trigger("resize");
 });
