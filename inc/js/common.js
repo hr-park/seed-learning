b16600e6393bc9499b413ddf36571cc04502c1ac
hr-park <34112814+hr-park@users.noreply.github.com>
2023-07-12 22:02:47 +0900

    test

--- inc/js/common.js
+++ inc/js/common.js
@@ -1,17 +1,31 @@
-/*퍼블용*/
+//alert이 필요하다면 swal("Hello world!"); 사용해주세요.
 $(document).ready( function() {
+	//퍼블 확인용
 	$("#header_wrap").load("./inc/layout/header.html");
 	$("#floating_wrap").load("./inc/layout/floating_sns.html");
 	$("#footer_wrap").load("./inc/layout/footer.html");
+	//퍼블 확인용
 
 	$('.tabs li a').click(function(){
 		let idx = $(this).parent('li').index()+1;
-		console.log(idx)
-
 
 		$('.tabs li.active').removeClass('active');
 		$(this).parent('li').addClass('active');
 		$('.tab_box').hide();
 		$('.tab_box[data-tab="'+idx+'"]').show();
 	});
+
+	$(document).on('click', ".btn_like", function(){
+		if($(this).hasClass('active')){
+			swal("나의 관심 교재에서 삭제되었습니다!");
+			$(this).removeClass('active');
+		}else{
+			swal("나의 관심 교재에 등록되었습니다!");
+			$(this).addClass('active');
+		}
+	});
 });
+
+function autoHyphen(target){
+	target.value = target.value.replace(/[^0-9]/g, '').replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`).replace(/(\-{1,2})$/g, "");
+}
